eab756cce6143cbea8ee548646d094a9d05555c7
AircityScript <842736610@qq.com>
2015-11-04 09:25:05 +0800

    commit

---
+++ client/compatibility/startup.js
@@ -0,0 +1,12 @@
+// Meteor.startup(function () {
+//   AccountsEntry.config({
+//     extraSignUpFields: [{ // Add extra signup fields on the signup page
+//       field: "email", // The database property you want to store the data in
+//       name: "email", // An initial value for the field, if you want one
+//       label: "邮箱", // The html lable for the field
+//       placeholder: "邮箱", // A placeholder for the field
+//       type: "text", // The type of field you want
+//       required: true // Adds html 5 required property if true
+//    }]
+//   });
+// })
