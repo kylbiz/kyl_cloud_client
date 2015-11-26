7a774228ca2626314c80b7fa8ff97a57da20265f
AircityScript <842736610@qq.com>
2015-11-26 14:28:36 +0800

    i18

---
+++ client/lib/dataTableFn.js
@@ -0,0 +1,18 @@
+ $.extend($.fn.dataTable.defaults, {
+        "language": {
+        "processing": "正在加载中...", 
+        "lengthMenu": "每页显示 _MENU_ 条记录",
+        "zeroRecords": "无数据显示",
+        "info": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+        "infoEmpty": "暂无数据",
+        "sEmptyTable":"暂无数据",
+        "infoFiltered": "(从 _MAX_ 条数据中检索)",
+        "search":"搜索",        
+        "paginate": {
+            "first": "首页",
+            "previous": "前一页",
+            "next": "后一页",
+            "last": "最后一页"
+            }
+       }
+  });
